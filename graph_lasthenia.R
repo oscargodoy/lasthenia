d9aa255ac4185147f9157d4c09affa7b8c5447a3
Oscar Godoy <ogodoy.re@gmail.com>
2017-05-14 17:13:59 +0200

    graph done

--- graph_lasthenia.R
+++ graph_lasthenia.R
@@ -2,3 +2,24 @@
 #loading the data for phenology and height
 phe<-read.table('Plot_Fenologia.txt', header=T, sep="\t")
 alt<-read.table('Plot_Altura.txt', header=T, sep="\t")
+
+#First phenology and later height
+par(mfrow=c(1,2))
+
+attach(phe)
+plot(Niche.differences[1:3], log(1/Fitness.differences[1:3] + 1), pch=16, lwd=1, xlab="Niche differences", ylab="Fitness differences (log. transformed)", main= "Phenology", ylim=c(0,8), xlim=c(0,1))
+points(Niche.differences[4:6], log(1/Fitness.differences[4:6]), pch=1, lwd=1)
+curve(log(1/(1-x)), add=T, col="red", lwd=3)
+legend(0, 8, c("Bromus", "Lactuca"), col = c(1, 1), lwd=c(1,1), lty=c(0,0),  pch=c(16,1), bty="n",
+       merge = TRUE)
+detach(phe)
+
+attach(alt)
+plot(Niche.differences[1:3], log(1/Fitness.differences[1:3]), pch=16, lwd=1, xlab="Niche differences", ylab=" ", main= "Height", ylim=c(0,8), xlim=c(0,1))
+points(Niche.differences[4:6], log(1/Fitness.differences[4:6]), pch=1, lwd=1)
+curve(log(1/(1-x)), add=T, col="red", lwd=3)
+legend(0, 8, c("Bromus", "Lactuca"), col = c(1, 1), lwd=c(1,1), lty=c(0,0),  pch=c(16,1), bty="n",
+       merge = TRUE)
+detach(alt)
+
+
