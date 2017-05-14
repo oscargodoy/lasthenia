3392f8ee745ade2e86cc1f5a2c9244ae6f464aa6
Oscar Godoy <ogodoy.re@gmail.com>
2017-05-14 16:48:53 +0200

    adding data

---
+++ graph_lasthenia.R
@@ -0,0 +1,4 @@
+#graph for lasthenia Paper
+#loading the data for phenology and height
+phe<-read.table('Plot_Fenologia.txt', header=T, sep="\t")
+alt<-read.table('Plot_Altura.txt', header=T, sep="\t")
