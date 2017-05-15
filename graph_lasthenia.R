6b8708ca3a2fe567365c38fa4691fd3984067364
Oscar Godoy <ogodoy.re@gmail.com>
2017-05-15 09:08:39 +0200

    new

--- graph_lasthenia.R
+++ graph_lasthenia.R
@@ -12,6 +12,16 @@ points(Niche.differences[4:6], log(1/Fitness.differences[4:6]), pch=1, lwd=2)
 curve(log(1/(1-x)), add=T, col="red", lwd=3)
 legend(0, 8, c("Bromus", "Lactuca"), col = c(1, 1), lwd=c(1,1), lty=c(0,0),  pch=c(16,1), bty="n",
        merge = TRUE)
+#Bromus
+text(x=0.18, y=1.3, "Early")
+text(x=0.5, y=1.3, "Middle")
+text(x=0.4, y=0.9, "Late")
+
+#Lactuca
+text(x=0.87, y=5, "Early")
+text(x=0.87, y=4.65, "Middle")
+text(x=0.89, y=4.3, "Late")
+
 detach(phe)
 
 attach(alt)
@@ -20,6 +30,7 @@ points(Niche.differences[4:6], log(1/Fitness.differences[4:6]), pch=1, lwd=2)
 curve(log(1/(1-x)), add=T, col="red", lwd=3)
 legend(0, 8, c("Bromus", "Lactuca"), col = c(1, 1), lwd=c(2,2), lty=c(0,0),  pch=c(16,1), bty="n",
        merge = TRUE)
+text
 detach(alt)
 
 
