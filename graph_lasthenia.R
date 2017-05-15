e25f4abdacd7d953f14b4d023873557e09fb1f28
Oscar Godoy <ogodoy.re@gmail.com>
2017-05-15 08:19:05 +0200

    rehechos

--- graph_lasthenia.R
+++ graph_lasthenia.R
@@ -7,18 +7,18 @@ alt<-read.table('Plot_Altura.txt', header=T, sep="\t")
 par(mfrow=c(1,2))
 
 attach(phe)
-plot(Niche.differences[1:3], log(1/Fitness.differences[1:3] + 1), pch=16, lwd=1, xlab="Niche differences", ylab="Fitness differences (log. transformed)", main= "Phenology", ylim=c(0,8), xlim=c(0,1))
-points(Niche.differences[4:6], log(1/Fitness.differences[4:6]), pch=1, lwd=1)
+plot(Niche.differences[1:3], log(1/Fitness.differences[1:3] + 1), pch=16, lwd=1, xlab="Niche differences", ylab="Fitness differences (log. transformed)", main= "(A) Phenology", ylim=c(0,8), xlim=c(0,1))
+points(Niche.differences[4:6], log(1/Fitness.differences[4:6]), pch=1, lwd=2)
 curve(log(1/(1-x)), add=T, col="red", lwd=3)
 legend(0, 8, c("Bromus", "Lactuca"), col = c(1, 1), lwd=c(1,1), lty=c(0,0),  pch=c(16,1), bty="n",
        merge = TRUE)
 detach(phe)
 
 attach(alt)
-plot(Niche.differences[1:3], log(1/Fitness.differences[1:3]), pch=16, lwd=1, xlab="Niche differences", ylab=" ", main= "Height", ylim=c(0,8), xlim=c(0,1))
-points(Niche.differences[4:6], log(1/Fitness.differences[4:6]), pch=1, lwd=1)
+plot(Niche.differences[1:3], log(1/Fitness.differences[1:3]), pch=16, lwd=1, xlab="Niche differences", ylab=" ", main= "(B) Height", ylim=c(0,8), xlim=c(0,1))
+points(Niche.differences[4:6], log(1/Fitness.differences[4:6]), pch=1, lwd=2)
 curve(log(1/(1-x)), add=T, col="red", lwd=3)
-legend(0, 8, c("Bromus", "Lactuca"), col = c(1, 1), lwd=c(1,1), lty=c(0,0),  pch=c(16,1), bty="n",
+legend(0, 8, c("Bromus", "Lactuca"), col = c(1, 1), lwd=c(2,2), lty=c(0,0),  pch=c(16,1), bty="n",
        merge = TRUE)
 detach(alt)
 
